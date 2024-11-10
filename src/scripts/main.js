3db0c9030f4ef87f9f367288b6e1b7afe7df9762
Mario Netto <marionettojr@gmail.com>
2024-11-10 16:12:21 -0300

    atualizando js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,9 @@ const elemento = document.querySelector('.portfolio')
 const x = document.querySelector('.test')
 
 elemento.addEventListener('mouseover', function() {
-
     x.classList.add('test--mostra')
-    console.log(x)
+})
+
+elemento.addEventListener('mouseout', function() {
+    x.classList.remove('test--mostra')
 })
