81864d4e44ae753d483211b364b565e32dfb33d1
Mario Netto <marionettojr@gmail.com>
2024-11-11 19:46:22 -0300

    add classe scss mobile - mostra portfolio e desfoca fundo no mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,174 +1,182 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const aboutText = document.querySelector('.header__about__text')
+    const aboutLinks = document.querySelector('.header__about__links')
+    const videoPortfolio = document.querySelectorAll('.portfolio-video')
 
     showCuphead()
     showUmso()
     showAppTimer()
     showArchanjo()
     showForm()
+    
 
-    movePortfolio()
-    
-    
+    // if (window.innerWidth >= 767) {
+
+    // } 
+//---------------------------------------------------------------------------
+    function showCuphead () {
+        const elementButton = document.querySelector('.cuphead')
+        const elementVideo = document.querySelector('.portfolio-video-cuphead')
+        const linkPortfolio = document.querySelector('.portfolio')
+
+        if (window.innerWidth >= 767) {
+             //Mouse in
+            elementButton.addEventListener('mouseover', function() {
+                startVideo(elementVideo)
+                movePortfolio()
+                //hide about
+                aboutText.classList.add('opacity-none')
+                aboutLinks.classList.add('opacity-none')
+                //show portfolio
+                elementVideo.classList.remove('opacity-none')
+                elementVideo.classList.add('portfolio-video--show')
+
+                //mouse out
+                elementButton.addEventListener('mouseout', function() {
+                    //show about
+                    aboutText.classList.remove('opacity-none')
+                    aboutLinks.classList.remove('opacity-none')
+                    //hide portfolio
+                    elementVideo.classList.add('opacity-none')
+                    elementVideo.classList.remove('portfolio-video--show')
+                })
+            })
+        } else {
+            linkPortfolio.classList.add('disable-link')
+        }
+    }
+
+    function showUmso () {
+        const elementButton = document.querySelector('.appTimer')
+        const elementVideo = document.querySelector('.portfolio-video-appTimer')
+        
+        if (window.innerWidth >= 767) {
+            //Mouse in
+            elementButton.addEventListener('mouseover', function() {
+                startVideo(elementVideo)
+                movePortfolio()
+                //hide about
+                aboutText.classList.add('opacity-none')
+                aboutLinks.classList.add('opacity-none')
+                //show portfolio
+                elementVideo.classList.remove('opacity-none')
+                elementVideo.classList.add('portfolio-video--show')
+
+                //mouse out
+                elementButton.addEventListener('mouseout', function() {
+                    //show about
+                    aboutText.classList.remove('opacity-none')
+                    aboutLinks.classList.remove('opacity-none')
+                    //hide portfolio
+                    elementVideo.classList.add('opacity-none')
+                    elementVideo.classList.remove('portfolio-video--show')
+                })
+            })
+        }
+    }
+
+    function showAppTimer () {
+        const elementButton = document.querySelector('.umso')
+        const elementVideo = document.querySelector('.portfolio-video-umso')
+        
+        if (window.innerWidth >= 767) {
+            //Mouse in
+            elementButton.addEventListener('mouseover', function() {
+                startVideo(elementVideo)
+                movePortfolio()
+                //hide about
+                aboutText.classList.add('opacity-none')
+                aboutLinks.classList.add('opacity-none')
+                //show portfolio
+                elementVideo.classList.remove('opacity-none')
+                elementVideo.classList.add('portfolio-video--show')
+
+                //mouse out
+                elementButton.addEventListener('mouseout', function() {
+                    //show about
+                    aboutText.classList.remove('opacity-none')
+                    aboutLinks.classList.remove('opacity-none')
+                    //hide portfolio
+                    elementVideo.classList.add('opacity-none')
+                    elementVideo.classList.remove('portfolio-video--show')
+                })
+            })
+        }
+    }
+
+    function showArchanjo () {
+        const elementButton = document.querySelector('.archanjo')
+        const elementVideo = document.querySelector('.portfolio-video-archanjo')
+        
+        if (window.innerWidth >= 767) {
+            //Mouse in
+            elementButton.addEventListener('mouseover', function() {
+                startVideo(elementVideo)
+                movePortfolio()
+                //hide about
+                aboutText.classList.add('opacity-none')
+                aboutLinks.classList.add('opacity-none')
+                //show portfolio
+                elementVideo.classList.remove('opacity-none')
+                elementVideo.classList.add('portfolio-video--show')
+
+                //mouse out
+                elementButton.addEventListener('mouseout', function() {
+                    //show about
+                    aboutText.classList.remove('opacity-none')
+                    aboutLinks.classList.remove('opacity-none')
+                    //hide portfolio
+                    elementVideo.classList.add('opacity-none')
+                    elementVideo.classList.remove('portfolio-video--show')
+                })
+            })
+        }
+    }
+
+    function showForm () {
+        const elementButton = document.querySelector('.form')
+        const elementVideo = document.querySelector('.portfolio-video-form')
+        
+        if (window.innerWidth >= 767) {
+            //Mouse in
+            elementButton.addEventListener('mouseover', function() {
+                startVideo(elementVideo)
+                movePortfolio()
+                //hide about
+                aboutText.classList.add('opacity-none')
+                aboutLinks.classList.add('opacity-none')
+                //show portfolio
+                elementVideo.classList.remove('opacity-none')
+                elementVideo.classList.add('portfolio-video--show')
+
+                //mouse out
+                elementButton.addEventListener('mouseout', function() {
+                    //show about
+                    aboutText.classList.remove('opacity-none')
+                    aboutLinks.classList.remove('opacity-none')
+                    //hide portfolio
+                    elementVideo.classList.add('opacity-none')
+                    elementVideo.classList.remove('portfolio-video--show')
+                })
+            })
+        }
+    }
+
+    function movePortfolio() {
+        document.addEventListener('mousemove', function(e) {
+            const mouseX = e.pageX
+            const mouseY = e.pageY
+
+            videoPortfolio.forEach(function(e) {
+                e.style.left = `${mouseX / 60 - 5}%`
+                e.style.top = `${(mouseY / 60) + 10}%`
+            })
+        })
+    }
+
+    function startVideo(video) {
+        video.currentTime = 0
+        // video.play();
+    }
+
 })
-
-function showCuphead () {
-    const elementButton = document.querySelector('.cuphead')
-    const elementVideo = document.querySelector('.portfolio-video-cuphead')
-    const aboutText = document.querySelector('.header__about__text')
-    const aboutLinks = document.querySelector('.header__about__links')
-    
-        //Mouse in
-    elementButton.addEventListener('mouseover', function() {
-        startVideo(elementVideo)
-        //hide about
-        aboutText.classList.add('opacityNone')
-        aboutLinks.classList.add('opacityNone')
-        //show portfolio
-        elementVideo.classList.remove('opacityNone')
-        elementVideo.classList.add('portfolio-video--mostra')
-
-        //mouse out
-        elementButton.addEventListener('mouseout', function() {
-            //show about
-            aboutText.classList.remove('opacityNone')
-            aboutLinks.classList.remove('opacityNone')
-            //hide portfolio
-            elementVideo.classList.add('opacityNone')
-            elementVideo.classList.remove('portfolio-video--mostra')
-        })
-    })
-}
-
-function showUmso () {
-    const elementButton = document.querySelector('.appTimer')
-    const elementVideo = document.querySelector('.portfolio-video-appTimer')
-    const aboutText = document.querySelector('.header__about__text')
-    const aboutLinks = document.querySelector('.header__about__links')
-    
-        //Mouse in
-    elementButton.addEventListener('mouseover', function() {
-        startVideo(elementVideo)
-        //hide about
-        aboutText.classList.add('opacityNone')
-        aboutLinks.classList.add('opacityNone')
-        //show portfolio
-        elementVideo.classList.remove('opacityNone')
-        elementVideo.classList.add('portfolio-video--mostra')
-
-        //mouse out
-        elementButton.addEventListener('mouseout', function() {
-            //show about
-            aboutText.classList.remove('opacityNone')
-            aboutLinks.classList.remove('opacityNone')
-            //hide portfolio
-            elementVideo.classList.add('opacityNone')
-            elementVideo.classList.remove('portfolio-video--mostra')
-        })
-    })
-}
-
-function showAppTimer () {
-    const elementButton = document.querySelector('.umso')
-    const elementVideo = document.querySelector('.portfolio-video-umso')
-    const aboutText = document.querySelector('.header__about__text')
-    const aboutLinks = document.querySelector('.header__about__links')
-    
-        //Mouse in
-    elementButton.addEventListener('mouseover', function() {
-        startVideo(elementVideo)
-        //hide about
-        aboutText.classList.add('opacityNone')
-        aboutLinks.classList.add('opacityNone')
-        //show portfolio
-        elementVideo.classList.remove('opacityNone')
-        elementVideo.classList.add('portfolio-video--mostra')
-
-        //mouse out
-        elementButton.addEventListener('mouseout', function() {
-            //show about
-            aboutText.classList.remove('opacityNone')
-            aboutLinks.classList.remove('opacityNone')
-            //hide portfolio
-            elementVideo.classList.add('opacityNone')
-            elementVideo.classList.remove('portfolio-video--mostra')
-        })
-    })
-}
-
-function showArchanjo () {
-    const elementButton = document.querySelector('.archanjo')
-    const elementVideo = document.querySelector('.portfolio-video-archanjo')
-    const aboutText = document.querySelector('.header__about__text')
-    const aboutLinks = document.querySelector('.header__about__links')
-    
-        //Mouse in
-    elementButton.addEventListener('mouseover', function() {
-        startVideo(elementVideo)
-        //hide about
-        aboutText.classList.add('opacityNone')
-        aboutLinks.classList.add('opacityNone')
-        //show portfolio
-        elementVideo.classList.remove('opacityNone')
-        elementVideo.classList.add('portfolio-video--mostra')
-
-        //mouse out
-        elementButton.addEventListener('mouseout', function() {
-            //show about
-            aboutText.classList.remove('opacityNone')
-            aboutLinks.classList.remove('opacityNone')
-            //hide portfolio
-            elementVideo.classList.add('opacityNone')
-            elementVideo.classList.remove('portfolio-video--mostra')
-        })
-    })
-}
-
-function showForm () {
-    const elementButton = document.querySelector('.form')
-    const elementVideo = document.querySelector('.portfolio-video-form')
-    const aboutText = document.querySelector('.header__about__text')
-    const aboutLinks = document.querySelector('.header__about__links')
-    
-        //Mouse in
-    elementButton.addEventListener('mouseover', function() {
-        startVideo(elementVideo)
-        //hide about
-        aboutText.classList.add('opacityNone')
-        aboutLinks.classList.add('opacityNone')
-        //show portfolio
-        elementVideo.classList.remove('opacityNone')
-        elementVideo.classList.add('portfolio-video--mostra')
-
-        //mouse out
-        elementButton.addEventListener('mouseout', function() {
-            //show about
-            aboutText.classList.remove('opacityNone')
-            aboutLinks.classList.remove('opacityNone')
-            //hide portfolio
-            elementVideo.classList.add('opacityNone')
-            elementVideo.classList.remove('portfolio-video--mostra')
-        })
-    })
-}
-
-
-
-function movePortfolio() {
-    document.addEventListener('mousemove', function(e) {
-        const cursorFollows = document.querySelectorAll('.move')
-        const mouseX = e.pageX
-        const mouseY = e.pageY
-
-        cursorFollows.forEach(function(cursorFollow) {
-            cursorFollow.style.left = `${mouseX / 60 - 10}%`
-            cursorFollow.style.top = `${(mouseY / 60) + 10}%`
-        });
-    });
-}
-
-const video = document.querySelector('video');
-
-function startVideo(video) {
-    video.currentTime = 0;
-    // video.play();
-}
